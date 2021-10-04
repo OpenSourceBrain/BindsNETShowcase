2045b96d70cf6375a50a9b919196347da87d8fce
Padraig Gleeson <p.gleeson@gmail.com>
2021-10-04 18:53:42 +0100

    Update simplespiking.py

--- BindsNET/simplespiking.py
+++ BindsNET/simplespiking.py
@@ -1,2 +1,17 @@
 from bindsnet.network import Network
+from bindsnet.network.nodes import Input, LIFNodes
+from bindsnet.network.topology import Connection
+from bindsnet.encoding import poisson
+
+network = Network()
+network.add_layer(Input(n=n_neurons), name="X")
+network.add_layer(LIFNodes(n=n_neurons), name="Y")
+network.add_connection(
+    Connection(source=network.layers["X"], target=network.layers["Y"]),
+    source="X",
+    target="Y",
+)
+
+data = {"X": poisson(datum=torch.rand(n_neurons), time=time)}
+network.run(inputs=data, time=time)
 
