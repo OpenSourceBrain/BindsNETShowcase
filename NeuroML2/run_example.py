c61173bde4f5ed44a6b165709cef1b6642916052
Padraig Gleeson <p.gleeson@gmail.com>
2021-10-08 16:08:36 +0100

    Updated example

--- NeuroML2/run_example.py
+++ NeuroML2/run_example.py
@@ -1,5 +1,6 @@
 from neuromllite.NetworkGenerator import check_to_generate_or_run
 from neuromllite import Simulation
+import sys
 
 from neuromllite.utils import load_simulation_json
 
@@ -7,4 +8,5 @@ sim = load_simulation_json('SimTestNet.json')
 
 print('Going to run simulation in BindsNet: %s'%sim)
 
-check_to_generate_or_run(['-bindsnet'], sim)
+
+check_to_generate_or_run(sys.argv, sim) # try -bindsnet
